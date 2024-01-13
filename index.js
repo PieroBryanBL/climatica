8fb22659e6cf4e38f9710570ee909f41296f3949
PieroBryanBL <pierobryanst1@gmail.com>
2024-01-13 11:03:30 -0500

    HTTP Problem

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const API_URL = "http://api.weatherapi.com/v1";
+const API_URL = "https://api.weatherapi.com/v1";
 const API_KEY = "026926a12262483dbfb204157240701";
 const searchPlace = document.getElementById("searchPlace");
 const answerCountry = document.getElementById("country");
@@ -21,21 +21,3 @@ const search = () => {
       answerFeelsLike.textContent = `${data.current.feelslike_c}`;
     });
 };
-
-/* const requestCity = document.getElementById("city");
-const answer = document.getElementById("result");
-const API_URL = "http://api.weatherapi.com/v1";
-const API_KEY = "026926a12262483dbfb204157240701";
-
-const getWeather = () => {
-  const city = encodeURIComponent(requestCity.value);
-  fetch(`${API_URL}/current.json?key=${API_KEY}&q=${city}`)
-    .then((response) => response.json())
-    .then((data) => {
-      answer.innerHTML = `La temperatura actual en ${data.location.name} es ${data.current.temp_c}°C`;
-    })
-    .catch((error) => {
-      console.error("Error al obtener el clima:", error);
-    });
-};
- */
